304f5c51f9db9236a4ce7a3373265e04e2858a86
Atul Tyagi <atul22tyagi@gmail.com>
2025-04-11 06:49:49 +0000

    How Datatypes work

---
+++ .devcontainer/01_basic/02_dataTypes.js
@@ -0,0 +1,12 @@
+let String = "Atul"
+let Integer = 20
+let bollean =true
+null;
+
+
+console.log(typeof undefined);
+console.log(typeof String)
+console.log(typeof Integer)
+console.log(typeof bollean)
+console.log(typeof null)
+console.log(typeof Symbol)                  
