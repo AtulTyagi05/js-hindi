1f0256f768ebadbb504a0660e0f7f3548def518f
Atul Tyagi <atul22tyagi@gmail.com>
2025-04-15 07:06:08 +0000

    conversation of data types

---
+++ .devcontainer/01_basic/03_conversionofDatatypes.js
@@ -0,0 +1,12 @@
+let number = 33
+
+let stringNumber = String(number);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+let someString = "Atul";
+let someNUmber = Number(someString);
+console.log(someNUmber);
+console.log(typeof someNumber);
+
+
